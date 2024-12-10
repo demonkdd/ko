541152a5c70ff41779aa2a1497a26c0103be6aaf
C <chronocrusaderagain@yahoo.com>
2024-12-10 09:44:22 -0500

    Initial Commit

---
+++ ko/koApp.swift
@@ -0,0 +1,17 @@
+//
+//  koApp.swift
+//  ko
+//
+//  Created by Cameron Slaughter on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct koApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ koTests/koTests.swift
@@ -0,0 +1,17 @@
+//
+//  koTests.swift
+//  koTests
+//
+//  Created by Cameron Slaughter on 12/10/24.
+//
+
+import Testing
+@testable import ko
+
+struct koTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
